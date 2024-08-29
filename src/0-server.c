10c6b4047ecb2f5592f4ddba83ae8418de4eb711
diasbouk <boukdsaid@gmail.com>
2024-08-29 19:58:54 +0100

    infinte loop , might add signlas handling to properly terminate the server and cleanup things

--- src/0-server.c
+++ src/0-server.c
@@ -17,27 +17,29 @@ int main(void)
 		return (0);
 	}
 
-	clienfd = accept(sockfd, (struct sockaddr *)&address, &size);
-	if (clienfd == -1)
-		ft_failed("failed to accept !\n");
+	while (1)
+	{
+		clienfd = accept(sockfd, (struct sockaddr *)&address, &size);
+		if (clienfd == -1)
+			ft_failed("failed to accept !\n");
 
-	buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE);
-	ft_bzero(buffer, BUFFER_SIZE);
+		buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+		ft_bzero(buffer, BUFFER_SIZE);
 
-	if (read(clienfd, buffer, BUFFER_SIZE) < 1)
-		ft_failed("Error reading\n");
+		if (read(clienfd, buffer, BUFFER_SIZE) < 1)
+			ft_failed("Error reading\n");
 
-	buffer = ft_realloc(buffer, ft_strlen(buffer), BUFFER_SIZE);
-	req = strto_request(buffer);
-	response = create_response(req);
-	printf("\n\n --- %s --- \n\n", response);
-	write(clienfd, response, ft_strlen(response));
-
-	free(req->route);
-	free(req);
-	free(buffer);
-	free(response);
-	close(clienfd);
+		buffer = ft_realloc(buffer, ft_strlen(buffer), BUFFER_SIZE);
+		req = strto_request(buffer);
+		response = create_response(req);
+		printf("\n\n --- %s --- \n\n", response);
+		write(clienfd, response, ft_strlen(response));
+		free(req->route);
+		free(req);
+		free(buffer);
+		free(response);
+		close(clienfd);
+	}
 	close(sockfd);
 	return (0);
 }
