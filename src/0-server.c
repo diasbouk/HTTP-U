c05f1f510a2a8f73abeda1503b899eb61d57923d
diasbouk <boukdsaid@gmail.com>
2024-08-29 10:27:21 +0100

    Build

--- src/0-server.c
+++ src/0-server.c
@@ -1,43 +1,43 @@
 #include "../include/http.h"
 #include "../include/strings.h"
 
-int main(void) {
+int main(void)
+{
 
 	struct sockaddr_in address;
 	socklen_t size = sizeof(address);
-	int sockfd, clienfd, i = 0;
+	int sockfd, clienfd;
 	request_t *req;
 	char *buffer, *response;
 
 	sockfd = init_socket(&address);
-	if (sockfd == -1) {
+	if (sockfd == -1)
+	{
 		ft_puts("Sokcet messed up!\n");
 		return (0);
 	}
 
-	while (TRUE) {
-		clienfd = accept(sockfd, (struct sockaddr *)&address, &size);
-		if (clienfd == -1)
-			ft_failed("failed to accept !\n");
+	clienfd = accept(sockfd, (struct sockaddr *)&address, &size);
+	if (clienfd == -1)
+		ft_failed("failed to accept !\n");
 
-		buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE);
-		ft_bzero(buffer, BUFFER_SIZE);
+	buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+	ft_bzero(buffer, BUFFER_SIZE);
 
-		if (read(clienfd, buffer, BUFFER_SIZE) < 1)
-			ft_failed("Error reading\n");
+	if (read(clienfd, buffer, BUFFER_SIZE) < 1)
+		ft_failed("Error reading\n");
 
-		buffer = ft_realloc(buffer, ft_strlen(buffer), BUFFER_SIZE);
-		req = strto_request(buffer);
-		printf("methode : %d\n route: %s\n version: %d\n", req->method,
-			   req->route, req->version);
-		response = create_response(req);
-		free(req);
-		req = NULL;
-		write(clienfd, response, ft_strlen(response));
+	buffer = ft_realloc(buffer, ft_strlen(buffer), BUFFER_SIZE);
+	req = strto_request(buffer);
+	response = create_response(req);
+	printf("\n\n --- %s --- \n\n", response);
+	write(clienfd, response, ft_strlen(response));
 
-		close(clienfd);
-		free(buffer);
-	}
+	free(req->route);
+	free(req);
+	free(buffer);
+	free(response);
+	close(clienfd);
 	close(sockfd);
 	return (0);
 }
