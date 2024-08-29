8e2cfac5ee690a5218f4425ac0bf1576db297ad3
diasbouk <boukdsaid@gmail.com>
2024-08-29 10:25:16 +0100

    Memory bugs and valgrind fixed, still need to handle infinite loops

--- depend/create_response.c
+++ depend/create_response.c
@@ -1,31 +1,56 @@
 #include "../include/http.h"
 #include "../include/strings.h"
 
+
+char	*get_fname(char *route) {
+	char *fname;
+	char	*data = "data";
+	int size;
+	if (!route)
+		return (NULL);
+	if (route[0] == '/' && !route[1]) {
+		fname = (char *)malloc(sizeof(char) * 16);
+		ft_bzero(fname, 16);
+		ft_strcpy(fname, "data/index.html");
+		fname[15] = '\0';
+		return (fname);
+	}
+
+	size = ft_strlen(route) + ft_strlen(data) + 1;
+	fname = (char *)malloc(sizeof(char) * size);
+	if (!fname)
+		return (NULL);
+	ft_bzero(fname, size);
+	ft_strcpy(fname, data);
+	ft_strcat(fname, route);
+	fname[size - 1] = '\0';
+
+	return (fname);
+}
+
 char *create_response(request_t *req) {
-	char *response, *status, *content, *header, fname[30] = {0};
+	char *response, *content_type, *content, *header, *fname;
 	int size;
 
-	if (!strcmp(req->route, "/"))
-		strcpy(fname, "data/index.html");
-	else {
-		strcpy(fname, "data");
-		strcat(fname, req->route);
-	}
+	fname = get_fname(req->route);
 	content = file_read(fname);
+	free(fname);
 	if (!content) {
-		header = "HTTP/1.1 404 NOT FOUND\r\n";
+		header = HEADER_404;
+		content_type =TYPE_TEXT;
 		content = file_read("data/not-found.html");
 	}
-	else
-		header = "HTTP/1.1 200 OK\r\n";
-	status = "Content-Type: text/html\r\n\r\n";
-	size = ft_strlen(header) + ft_strlen(status) + ft_strlen(content);
+	else {
+		header = HEADER_200;
+	content_type =TYPE_HTML;
+	}
+	size = ft_strlen(header) + ft_strlen(content_type) + ft_strlen(content);
 	response = malloc(sizeof(char) * (size + 1));
 	if (!response)
 		return (NULL);
 	ft_bzero(response, size);
 	strcpy(response, header);
-	strcat(response, status);
+	strcat(response, content_type);
 	strcat(response, content);
 	free(content);
 	return (response);
